2f7611a1193050cf54f3dece788067ed4b02751c
KishlayAmbastha14 <kishlaykumar087@gmail.com>
2025-12-24 22:40:38 +0530

    modified new api

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from typing import List,Literal,Annotated
 import pandas as pd
 import joblib
+from fastapi import Query
 
 
 
@@ -12,13 +13,22 @@ from preprocess import DropAndClip
 __main__.DropAndClip = DropAndClip
 
 
-# with open('dt_.joblib','rb') as f:
-#     model = joblib.load(f)
 
-# with open('decision_tree.joblib','rb') as f:
-#     model = joblib.load(f)
 
-decision_tree = joblib.load("decision_tree.joblib")
+dt = joblib.load("decision_tree.joblib")
+rf = joblib.load("pipeline_rf_tree.joblib")
+
+knn = joblib.load("pipeline_knn.joblib")
+lr = joblib.load("pipeline_lr.joblib")
+
+ada = joblib.load("pipeline_ada.joblib")
+cat = joblib.load("pipeline_cat.joblib")
+
+svc = joblib.load("pipeline_svm.joblib")
+xgb = joblib.load("xgb_.joblib")
+
+gb = joblib.load("pipeline_gb1.joblib")
+
 
 app = FastAPI(title="Web Intrusion Detection API")
 
@@ -192,16 +202,153 @@ class IntrusionRequest(BaseModel):
 async def main():
   return {"hello to fastapi backend"}
 
-@app.post("/decision_tree_predict")
-async def decision_tree_model(data:IntrusionRequest):
-    data = pd.DataFrame([data.model_dump()])
-    prediction = int(decision_tree.predict(data)[0])
-    probability = float(decision_tree.predict_proba(data).max())
 
-    return JSONResponse(
-        status_code=200,
-        content={
-            "prediction":prediction,
-            "confidence":round(float(probability),4)
-         }
-    )
+model_registry = {
+   "lr" : lr,
+   "knn" : knn,
+   "dt" : dt,
+   "rf" : rf,
+   "svc" : svc,
+   "cat" : cat,
+   "xgb" : xgb,
+   "ada" : ada,
+   "gb" : gb
+}
+
+@app.post("/predict")
+async def predict_intrusion(data:IntrusionRequest,
+                            model:Literal["lr","knn","dt","rf","svc","cat","xgb","ada","gb"] = 
+                            Query(
+                               default="xgb",
+                               description="select ml model for predictions"
+                            )):
+    
+    df = pd.DataFrame([data.model_dump()])
+    selected_model = model_registry.get(model.lower())
+
+
+    if selected_model is None:
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Invalid model choice"}
+        )
+    if(selected_model == 'svc'):
+        prediction = int(selected_model.predict(df)[0])
+        probability = float(xgb.predict_proba(df).max())
+    else:
+        prediction = int(selected_model.predict(df)[0])
+        probability = float(xgb.predict_proba(df).max())
+
+
+    return {
+        "model_used": model,
+        "prediction": prediction,
+        "confidence": round(probability, 4)
+    }
+
+
+# prediction = int(svc.predict(data)[0])
+# #     probability = float(xgb.predict_proba(data).max())
+
+# #     return JSONResponse(
+# #         status_code=200,content={
+# #             "prediction":prediction,
+# #             "confidence_matrix":round(float(probability),4)
+# #         }
+# #     )
+
+# @app.post("/decision_tree_predict")
+# async def decision_tree_model(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(dt.predict(data)[0])
+#     probability = float(dt.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,
+#         content={
+#             "prediction":prediction,
+#             "confidence":round(float(probability),4)
+#          }
+#     )
+
+# @app.post("/randomforest")
+# async def random_forest(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(rf.predict(data)[0])
+#     probability = float(rf.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,content = {
+#             "prediction": prediction,
+#             "confidence_meter":round(float(probability),4)
+#         }
+#     )
+
+
+
+# @app.post("/lregression")
+# async def lr_regression(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(lr.predict(data)[0])
+#     probability = float(lr.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,content = {
+#             "prediction": prediction,
+#             "confidence_meter":round(float(probability),4)
+#         }
+#     )
+
+
+# @app.post("/knnneigbour")
+# async def knn_neighbour(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(knn.predict(data)[0])
+#     probability = float(knn.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,content = {
+#             "prediction": prediction,
+#             "confidence_meter":round(float(probability),4)
+#         }
+#     )
+
+
+# @app.post("/catboost")
+# async def cat_boost(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(cat.predict(data)[0])
+#     probability = float(cat.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,content = {
+#             "prediction": prediction,
+#             "confidence_meter":round(float(probability),4)
+#         }
+#     )
+
+# @app.post("/xgbboost")
+# async def xgb_boost(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(xgb.predict(data)[0])
+#     probability = float(xgb.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,content = {
+#             "prediction": prediction,
+#             "confidence_meter":round(float(probability),4)
+#         }
+#     )
+
+# @app.post("/support_vector_classifier")
+# async def support_vector_classifier(data:IntrusionRequest):
+#     data = pd.DataFrame([data.model_dump()])
+#     prediction = int(svc.predict(data)[0])
+#     probability = float(xgb.predict_proba(data).max())
+
+#     return JSONResponse(
+#         status_code=200,content={
+#             "prediction":prediction,
+#             "confidence_matrix":round(float(probability),4)
+#         }
+#     )
